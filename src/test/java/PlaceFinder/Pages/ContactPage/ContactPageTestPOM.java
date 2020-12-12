10e0a21abb6b3ad0af2c3d21d24b8e6fe59b6c8b
istvangercsak <istvan.gercsak@gmail.com>
2020-12-12 18:25:08 +0000

    I built up the Contact page POM

--- src/test/java/PlaceFinder/Pages/ContactPage/ContactPageTestPOM.java
+++ src/test/java/PlaceFinder/Pages/ContactPage/ContactPageTestPOM.java
@@ -1,6 +1,8 @@
 package PlaceFinder.Pages.ContactPage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ContactPageTestPOM {
 
@@ -14,6 +16,40 @@ public class ContactPageTestPOM {
     String messageInputID = "description";
     String sendMessageButtonCSS = "body > section > div > div.col-lg-12.form-container > form > button";
 
+    String githubIconCSS = "body > footer > div > div.col-lg-6.list-inline.social-links-container > ul > li:nth-child(1) > a";
+    String linkedinIconCSS = "body > footer > div > div.col-lg-6.list-inline.social-links-container > ul > li:nth-child(2) > a";
     String githubIconHref = "https://github.com/IstvanGercsak";
     String linkedinIconHref = "https://www.linkedin.com/in/istvangercsak/";
+
+    WebElement goBackIconCSS(WebDriver driver) {
+        return driver.findElement(By.cssSelector(goBackIconCSS));
+    }
+
+    WebElement nameInputID(WebDriver driver) {
+        return driver.findElement(By.id(nameInputID));
+    }
+
+    WebElement nameInputEmailID(WebDriver driver) {
+        return driver.findElement(By.id(nameInputEmailID));
+    }
+
+    WebElement phoneNumberID(WebDriver driver) {
+        return driver.findElement(By.id(phoneNumberID));
+    }
+
+    WebElement messageInputID(WebDriver driver) {
+        return driver.findElement(By.id(messageInputID));
+    }
+
+    WebElement sendMessageButtonCSS(WebDriver driver) {
+        return driver.findElement(By.id(sendMessageButtonCSS));
+    }
+
+    WebElement githubIconCSS(WebDriver driver) {
+        return driver.findElement(By.cssSelector(githubIconCSS));
+    }
+
+    WebElement linkedinIconCSS(WebDriver driver) {
+        return driver.findElement(By.cssSelector(linkedinIconCSS));
+    }
 }
